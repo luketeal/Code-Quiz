2aa9cdd24f8c1ead1575a217d71afad6e970d905
Luke Teal <l.teal21@gmail.com>
2021-03-30 13:58:51 -0400

    Update JavaScript add pseudo code declared query selectors declared event listeners began coding start game

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+let startButton = document.querySelector(".begin");
+let answerButton = document.querySelector(".answer");
+let timeLeft = document.querySelector(".timer")
+let startScreen = document.querySelector(".start")
+
+let questions = {
+    question: ["question 0", "question 1"],
+    correctAnswer: ["correct 0", "correct 1"],
+    wrongAnswer: [
+        ["wrong 0.0", "wrong 0.1", "wrong 0.2"], 
+        ["wrong 1.0", "wrong 1.1", "wrong 1.2"]
+    ],
+    questionTracker: 0 
+}
+
+function cycleQuestions () {
+    
+}
+
+function runTimer () {
+
+}
+
+
+function startQuiz () {
+    // change display to none
+    startScreen.setAttribute("style", "display: none")
+    // let printOut = startButton.textContent
+    // console.log(printOut)
+    // startButton.textContent = "test";
+    cycleQuestions()
+    runTimer()
+}
+
+
+startButton.addEventListener("click", startQuiz)
+
+// event listener on begin game: when selected
+// change display to none
+// display first questions
+// start timer
+
+// event listener on answer buttons: when an answer is selected
+// evaluate if the answer was correct 
+    // if correct,
+        // tell me that the answer is correct
+    // if wrong,
+        // tell me that the answer is wrong and reduce time on the timer
+// display the next question
+
+// when last answer is selected
+    // display time left
+    // request initials
+    // save info to local storage
+
