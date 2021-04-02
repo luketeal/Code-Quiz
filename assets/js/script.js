a47cd966bbbbc74504017b757ba704a009827a35
Luke Teal <l.teal21@gmail.com>
2021-04-01 23:02:17 -0400

    Comment and clean JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Declare query selectors
 let startButton = document.querySelector(".begin");
 let answerButton = document.querySelectorAll(".answer");
 let timeLeft = document.querySelector(".timer");
@@ -11,6 +12,7 @@ let highScore = document.querySelector(".highScore");
 let initialsEl = document.querySelector("#initials");
 let scoreList = document.querySelector(".scoreList");
 
+// Define Array of Questions
 let questions = {
     question: ["question 0", "question 1", "question 2", "question 3"],
     correctAnswer: ["correct 0", "correct 1", "correct 2", "correct 3"],
@@ -24,59 +26,74 @@ let questions = {
     answerTracker: 0,
 }
 
-let questionTotal = questions.question.length - 1;
+// initialize the amount of time left for the quiz
+let secondsLeft = 50;
 
-let questionAnswers = [];
-
-let secondsLeft = 20;
-
+// initialize score variabl globally
 let score;
 
+// initialize the number of correct answers counter globally
 let correctCounter = 0;
 
+// initialize the high score array globally as an array of objects
 let highScoreArray = [{}];
-let highScoreArraySorted = [{}];
+
 
 function logScore () {
+
+    // toggle screens
     questionScreen.setAttribute("style", "display: none")
     scoreScreen.setAttribute("style", "display: block")
+
+    // calculate score
     score = correctCounter/questions.question.length*100
+
+    // display score
     quizScore.textContent = "Your Score: " + score + '%';
+
 }
 
 function cycleQuestions (event) {
 
+    // evaluate if the button pressed is undefined --> this will be undefined if the "begin" button is pressed 
+    // if it is undefined, there is no answer to be validated so this part of the function will be skipped
     if(event !== undefined) {
+        
+        // evaluate if the button pressed is the correct answer, if so increment the correct answer counter, if not reduce timer.
         if(event.target.textContent === questions.correctAnswer[questions.answerTracker]) {
-            questions.answerTracker++;
             correctCounter++;
         } else {
-            questions.answerTracker++;
             secondsLeft = secondsLeft-10;
+
+            // if timer is less than zero go to log score screen
             if(secondsLeft < 0) {
                 timeLeft.textContent = 'Done';
                 logScore();
             } else {
                 timeLeft.textContent = secondsLeft;
             }
-            
         }
+        questions.answerTracker++;
     }
 
     // cycles to the next question if there are still questions to answer and there is time left
-    if (questions.questionTracker <= questionTotal) {
+    if (questions.questionTracker < questions.question.length) {
 
+        // set the content of the question
         questionText.textContent = questions.question[questions.questionTracker];
        
-        questionAnswers = questions.wrongAnswer[questions.questionTracker];
+        // set the possible answers and randomize their order
+        let questionAnswers = questions.wrongAnswer[questions.questionTracker];
         questionAnswers.push(questions.correctAnswer[questions.questionTracker]);
         questionAnswers = questionAnswers.sort(() => Math.random() - 0.5);
         
+        // display the answers
         answerButton[0].textContent = questionAnswers[0];
         answerButton[1].textContent = questionAnswers[1];
         answerButton[2].textContent = questionAnswers[2];
         answerButton[3].textContent = questionAnswers[3];
         
+        // increment the question tracker for the next question
         questions.questionTracker++;
 
     } else {
@@ -88,9 +105,11 @@ function cycleQuestions (event) {
 
 function startQuiz () {
 
+    // toggle screens
     startScreen.setAttribute("style", "display: none");
     questionScreen.setAttribute("style", "display: flex");
-
+    
+    // begin timer
     timeLeft.textContent = secondsLeft;
     let timerInterval = setInterval(
         
@@ -111,6 +130,7 @@ function startQuiz () {
 
     );
 
+    // start first question
     cycleQuestions();
 
 }
@@ -130,7 +150,7 @@ function saveScore(event) {
         gameScore: score,
     }
 
-    // appends the user object (initials and score) to array of scores (highScoreArray)
+    // append the user object (initials and score) to array of scores (highScoreArray)
     if (JSON.parse(localStorage.getItem('highScores')) === null) {
         highScoreArray [0] = user
 
@@ -154,7 +174,7 @@ function saveScore(event) {
     }
 }
 
-
+// event listeners
 startButton.addEventListener("click", startQuiz);
 answerButton[0].addEventListener("click", cycleQuestions);
 answerButton[1].addEventListener("click", cycleQuestions);
